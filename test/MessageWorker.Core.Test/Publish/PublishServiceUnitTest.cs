2571b87a9392ba11cdc6f5cbbc4d3f4125c0e6f7
mukmyash <mukminovartur@gmail.com>
2018-07-14 00:21:27 +0500

    Add unit test for Publisher and PublishService

---
+++ test/MessageWorker.Core.Test/Publish/PublishServiceUnitTest.cs
@@ -0,0 +1,82 @@
+﻿using FakeItEasy;
+using MessageWorker.Abstractions;
+using MessageWorker.Core.Publish;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MessageWorker.Core.Test.Publish
+{
+    public class PublishServiceUnitTest
+    {
+        [Fact]
+        public void PublishService_GetOptionByTopicName()
+        {
+            var moqProducerSettings = new ProducerSettings()
+            {
+                Producer = A.Fake<IProducer>()
+            };
+
+            var moqOptionsSnapshot = A.Fake<IOptionsSnapshot<PublishSettings<string>>>();
+            A.CallTo(() => moqOptionsSnapshot.Get(A<string>.That.Not.IsEqualTo("topicName")))
+                .Throws(new Exception("Not valid copnfiguration name."));
+
+            var publishSerivce =
+                new PublishService<string>(moqProducerSettings, moqOptionsSnapshot, "topicName");
+        }
+
+        [Fact]
+        public void PublishService_Send_CallSerializer()
+        {
+            var moqProducerSettings = new ProducerSettings()
+            {
+                Producer = A.Fake<IProducer>()
+            };
+
+            var moqSerializer = A.Fake<ISerializer<string>>();
+            var moqPublishSettings = new PublishSettings<string>()
+            {
+                Serializer = moqSerializer
+            };
+            var moqOptionsSnapshot = A.Fake<IOptionsSnapshot<PublishSettings<string>>>();
+            A.CallTo(() => moqOptionsSnapshot.Get(A<string>._)).Returns(moqPublishSettings);
+
+            var publishSerivce =
+                new PublishService<string>(moqProducerSettings, moqOptionsSnapshot, "topicName");
+
+            publishSerivce.Send("test msg");
+
+            A.CallTo(() => moqSerializer.Serialize("test msg")).MustHaveHappenedOnceExactly();
+        }
+
+        [Fact]
+        public void PublishService_Send_CallProducer()
+        {
+            var moqProducer = A.Fake<IProducer>();
+            var moqProducerSettings = new ProducerSettings()
+            {
+                Producer = moqProducer
+            };
+
+            var moqSerializer = A.Fake<ISerializer<string>>();
+            A.CallTo(() => moqSerializer.Serialize(A<string>._))
+                .Returns(new byte[] { 0x0, 0x1, 0x2 });
+            var moqPublishSettings = new PublishSettings<string>()
+            {
+                Serializer = moqSerializer
+            };
+            var moqOptionsSnapshot = A.Fake<IOptionsSnapshot<PublishSettings<string>>>();
+            A.CallTo(() => moqOptionsSnapshot.Get(A<string>._)).Returns(moqPublishSettings);
+
+            var publishSerivce
+                = new PublishService<string>(moqProducerSettings, moqOptionsSnapshot, "topicName");
+
+            publishSerivce.Send("test msg");
+
+            A.CallTo(() => moqProducer.Send("topicName", A<byte[]>.That.IsSameSequenceAs(new byte[] { 0x0, 0x1, 0x2 })))
+            .MustHaveHappenedOnceExactly();
+        }
+    }
+}
---
+++ test/MessageWorker.Core.Test/Publish/PublisherUnitTest.cs
@@ -0,0 +1,79 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using MessageWorker.Abstractions;
+using MessageWorker.Core.Publish;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MessageWorker.Core.Test.Publish
+{
+    public class PublisherUnitTest
+    {
+        [Fact]
+        public void Publisher_RegisterOptionsByTopic()
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            var testClass = new Publisher(services, settings => { settings.Producer = null; });
+            testClass.Register<string>("topicName1", options => { options.Serializer = A.Fake<ISerializer<string>>(); });
+
+            var provider = services.BuildServiceProvider();
+            var optionSnapshot = provider.GetRequiredService<IOptionsSnapshot<PublishSettings<string>>>();
+
+            var options1 = optionSnapshot.Get("topicName1");
+            options1.Should().NotBeNull();
+            options1.Should().BeOfType<PublishSettings<string>>()
+            .Which.Serializer.Should().NotBeNull();
+
+            var options2 = optionSnapshot.Get("topicName2");
+            options2.Should().NotBeNull();
+            options2.Should().BeOfType<PublishSettings<string>>()
+                 .Which.Serializer.Should().BeNull();
+        }
+
+        [Fact]
+        public void Publisher_RegisterIPublishService()
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            var testClass = new Publisher(services, settings => { settings.Producer = null; });
+            testClass.Register<string>("topicName1", options => { options.Serializer = A.Fake<ISerializer<string>>(); });
+
+            var provider = services.BuildServiceProvider();
+            var publishSerivce = provider.GetRequiredService<IPublishService<string>>();
+
+            publishSerivce.Should().NotBeNull()
+                .And.BeOfType<PublishService<string>>()
+                .Which._topicName.Should().Be("topicName1");
+        }
+
+
+        [Fact]
+        public void Publisher_RegisterIPublishServiceMultiple()
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            var testClass = new Publisher(services, settings => { settings.Producer = null; });
+            testClass
+                .Register<string>("topicName1", options => { options.Serializer = A.Fake<ISerializer<string>>(); })
+                .Register<int>("topicName2", options => { options.Serializer = A.Fake<ISerializer<int>>(); });
+
+            var provider = services.BuildServiceProvider();
+
+            var publishSerivceString = provider.GetRequiredService<IPublishService<string>>();
+            publishSerivceString.Should().NotBeNull()
+                .And.BeOfType<PublishService<string>>()
+                .Which._topicName.Should().Be("topicName1");
+
+
+            var publishSerivceInt = provider.GetRequiredService<IPublishService<int>>();
+            publishSerivceInt.Should().NotBeNull()
+                .And.BeOfType<PublishService<int>>()
+                .Which._topicName.Should().Be("topicName2");
+        }
+    }
+}
