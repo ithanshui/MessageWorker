c55ae03d1426ac9b124d82899d322879125643c6
mukmyash <mukminovartur@gmail.com>
2018-07-17 00:07:16 +0500

    Feature/tests (#2)
    * Add Publisher. And Fix .gitignore
    
    * Add unit test for Publisher and PublishService
    
    * add tests for PublishService and Subscriber
    
    * add unit tests for SubscribeHostedService

---
+++ src/MessageWorker.Core/Publish/Publisher.cs
@@ -0,0 +1,40 @@
+﻿using MessageWorker.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MessageWorker.Core.Publish
+{
+    internal class Publisher : IPublisher
+    {
+        IServiceCollection _services;
+        Action<ProducerSettings> _producerSettingsAction;
+
+        public Publisher(IServiceCollection services, Action<ProducerSettings> producerSettingsAction)
+        {
+            _services = services;
+            _producerSettingsAction = producerSettingsAction;
+        }
+
+        public IPublisher Register<TMessage>(string topicName, Action<PublishSettings<TMessage>> options)
+        {
+            _services.Configure<PublishSettings<TMessage>>(topicName, options);
+
+            _services.AddScoped<IPublishService<TMessage>>(provider =>
+            {
+                var producerSettings = new ProducerSettings();
+                _producerSettingsAction?.Invoke(producerSettings);
+                var subscribeOptions = provider.GetRequiredService<IOptionsSnapshot<PublishSettings<TMessage>>>();
+
+                return new PublishService<TMessage>(
+                    producerSettings,
+                    subscribeOptions,
+                    topicName
+                    );
+            });
+            return this;
+        }
+    }
+}
